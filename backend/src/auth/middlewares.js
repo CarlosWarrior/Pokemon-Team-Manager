bdc7cf216600245e3234b7cc993d5adf8360125c
Saúl Napoleón Haro Hernández <saulnharoh@gmail.com>
2023-10-23 14:07:51 -0600

    created auth middlewares

--- backend/src/auth/middlewares.js
+++ backend/src/auth/middlewares.js
@@ -1,31 +1,38 @@
 const { raise } = require("../middlewares/errors")
 const { User, Admin } = require("./models")
+const { decode } = require("./crypto")
 function parseToken(token){
-    return token;
+    let decoded;
+    try {
+        decoded = decode(token);
+    } catch (error) {
+        return false;
+    }
+    return decoded;
 }
 const AuthMiddlewares = {
     user: async(req, res, next) => {
         if(!req.headers.token)
-            return raise({status:400})
+            return raise({status:400, message: 'No token'})
         const token = parseToken(req.headers.token)
         if(!token)
-            return raise({status: 401})
-        req.locals = {
-            token,
-            user: 'user'
-        }
+            return raise({status: 401, message: 'Invalid token'})
+        const user = await User.findOne({where: {email: token.email}})
+        if(!user)
+            return raise({status: 401, message: 'User not found'})
+        req.locals = token
         next()
     },
     admin: async(req, res, next) => {
         if(!req.headers.token)
-            return raise({status:400})
+            return raise({status:400, message: 'No token'})
         const token = parseToken(req.headers.token)
         if(!token)
-            return raise({status: 401})
-        req.locals = {
-            token,
-            admin: 'admin'
-        }
+            return raise({status: 401, message: 'Invalid token'})
+        const admin = await Admin.findOne({where: {email: token.email}})
+        if(!admin)
+            return raise({status: 401, message: 'Admin not found'})
+        req.locals = token
         next()
     }
 }
