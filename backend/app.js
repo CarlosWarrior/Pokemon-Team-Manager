eb7f622605586b176a7e6d206bc1f412602d957c
Carlos Alejandro Guerrero Martínez <carlos.alejandro.97@gmail.com>
2023-10-23 13:36:37 -0600

    added helmet

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,26 @@ require('dotenv').config()
 const express = require("express")
 const app = express()
 app.use(express.json())
+app.use(require('helmet')({
+    contentSecurityPolicy:{
+      directives: {
+        defaultSrc: ["'self'", 'data:', 'blob:',],
+    
+        fontSrc: ["'self'", 'https:', 'data:', "'unsafe-inline'"],
+    
+        scriptSrc: ["'self'",'https:', "'unsafe-inline'"],
+    
+        scriptSrcElem: ["'self'",'https:','https:', "'unsafe-inline'"],
+    
+        styleSrc: ["'self'", 'https:', "'unsafe-inline'"],
+    
+        connectSrc: ["'self'", 'data','https:']
+      },
+    }
+  }))
+  app.use(require('cors')({origin:'*'}))  
 
-if(!process.env.production<1){
+if(process.env.production!=1){
     const swagger = require('swagger-ui-express')
     const swaggerConfig = require('swagger-jsdoc')( require('./swagger.config') )
     app.use('/docs', swagger.serve, swagger.setup(swaggerConfig) )
