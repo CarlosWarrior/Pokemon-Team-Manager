1afb2b4e79cee17f18b6b2a12bd2778b9e1c26cb
ElAx7 <125324367+ElAx7@users.noreply.github.com>
2023-10-24 14:20:40 -0600

    implemented encryption and decryption

--- backend/src/auth/crypto.js
+++ backend/src/auth/crypto.js
@@ -1,5 +1,11 @@
 const { genSaltSync, hashSync, compare } = require('bcrypt')
 const { sign, verify } = require('jsonwebtoken')
+const { createCipheriv,  createDecipheriv, randomBytes } = require('crypto') 
+const secretkey = process.env.cipher_key
+const ivkey = process.env.iv_key
+const algorithm = 'aes-256-cbc'
+const key = Buffer.from(secretkey, 'utf-8');
+const iv = Buffer.from(ivkey, 'utf-8');
 
 const hash = text => hashSync(text, genSaltSync(16), null)
 const compareHash = (text, pass) => new Promise(resolve => compare(text, pass, (b,attempt) => resolve(attempt)))
@@ -7,9 +13,25 @@ const compareHash = (text, pass) => new Promise(resolve => compare(text, pass, (
 const tokenize = value => sign(value, process.env.key)
 const decode = value => verify(value, process.env.key)
 
+
+const encrypt = (message) => {
+  const cipher = createCipheriv(algorithm, key, iv);
+  return `${cipher.update(message, "utf-8", "hex")}${cipher.final("hex")}`;
+};
+
+const decrypt = (message) => {
+  const decipher = createDecipheriv(algorithm, key, iv);
+  return `${decipher.update(message, "hex", "utf-8")}${decipher.final(
+    "utf-8"
+  )}`;
+};
+
+
 module.exports = {
     hash,
     compareHash,
     tokenize,
-    decode
+    decode,
+    encrypt,
+    decrypt
 }
