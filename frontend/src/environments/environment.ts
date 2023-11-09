a0f6b69e088bd48ef90b435e5abeabd1848a68d5
Carlos Alejandro Guerrero Martínez <carlos.alejandro.97@gmail.com>
2023-11-09 08:29:51 -0600

    Added production configuration

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     google_api_client: "156274698762-kttcvch7t5dfi0rspjb31unrtakq7drc.apps.googleusercontent.com",
-    api: "https://pokemon.kewacagm.link",
+    api: "https://pokemon.kewacagm.link:3000",
 };
